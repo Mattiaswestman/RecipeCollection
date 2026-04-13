bf449e79f718998c0334d993c452ff67214a5eee
Mattias Westman <mattiaswestman@outlook.com>
2026-04-13 16:28:59 +0200

    Setup folder structure. Removed some sample code.

---
+++ recipe-collection/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace RecipeCollection.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
